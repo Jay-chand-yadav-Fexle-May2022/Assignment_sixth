7dc66368c578acc761d7a81ce7317b311cc93602
Jay Chand Yadav <105698104+Jay-chand-yadav-Fexle-May2022@users.noreply.github.com>
2022-09-02 10:09:36 +0530

    Update calculator.js

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -2,24 +2,20 @@ import { LightningElement } from 'lwc';
 export default class Calculator extends LightningElement {
     result
     showResult = false
-    // Operation will be perform during addition 
-    handleAdd(event){
-        this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  + parseInt(this.template.querySelectorAll(".operand")[1].value)
-        this.showResult = true     
-    }
-    // Operation will be perform during substration 
-    handleSub(event){
-        this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  - parseInt(this.template.querySelectorAll(".operand")[1].value)
+    // Perform arithmetic operation during action on html button
+    handleOperation(event){
+        if(event.target.title == 'addition'){
+            this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  + parseInt(this.template.querySelectorAll(".operand")[1].value)               
+        }
+        else if(event.target.title == 'subtract'){
+            this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  - parseInt(this.template.querySelectorAll(".operand")[1].value)
+        }
+        else if(event.target.title == 'multiply'){
+            this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  * parseInt(this.template.querySelectorAll(".operand")[1].value)
+        }
+        else{
+            this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  / parseInt(this.template.querySelectorAll(".operand")[1].value)
+        }
         this.showResult = true
-    }
-    // Operation will be perform during multiplication 
-    handleMul(event){
-        this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  * parseInt(this.template.querySelectorAll(".operand")[1].value)
-        this.showResult = true
-    }
-    // Operation will be perform during division 
-    handleDiv(event){
-        this.result = parseInt(this.template.querySelectorAll(".operand")[0].value)  / parseInt(this.template.querySelectorAll(".operand")[1].value)
-        this.showResult = true
-    }
+    }   
 }
