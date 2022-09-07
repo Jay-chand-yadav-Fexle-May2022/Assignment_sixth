6306e727219694013fd65a7d712327b180c716c1
Jay Chand Yadav <105698104+Jay-chand-yadav-Fexle-May2022@users.noreply.github.com>
2022-09-07 11:00:49 +0530

    Update accountRelatedrecord.js

--- src/components/accountRelatedrecord/accountRelatedrecord.js
+++ src/components/accountRelatedrecord/accountRelatedrecord.js
@@ -1,43 +1,51 @@
-import { LightningElement, track,  wire } from 'lwc';
+import { LightningElement, track} from 'lwc';
 import getContact from '@salesforce/apex/AccountRelated.contactRecord';
 import getOpp from '@salesforce/apex/AccountRelated.opportunityRecord';
 import getCase from '@salesforce/apex/AccountRelated.caseRecord';
+const conColumns = [
+          { label: 'LastName', fieldName: 'LastName' },
+          { label: 'Phone', fieldName: 'Phone', type: 'Phone' },
+          { label: 'Email', fieldName: 'Email', type: 'Email' }
+     ];
+const oppColumns = [
+          { label: 'Id', fieldName: 'Id' },
+          { label: 'Name', fieldName: 'Name'},
+          { label: 'StageName', fieldName: 'StageName' }
+     ];
+const caseColumns = [
+          { label: 'Id', fieldName: 'Id' },
+          { label: 'Status', fieldName: 'Status' },
+          { label: 'Priority', fieldName: 'Priority'}
+     ];
+
 export default class AccountRelatedrecord extends LightningElement {
+
+     conColumns = conColumns;
+     oppColumns = oppColumns;
+     caseColumns = caseColumns;
 
      @track contactList;
      @track opportunityList;
      @track caseList;
-     opp = false;
-     case = false;
+     showResult = false;
 
-     @wire(getContact) wiredContact({data,error}){
-          if (data) {
-               this.contactList = data;
-               this.opp = true;
-          console.log(data); 
-          } else if (error) {
-            console.log(error);
-          }
-     }    
-     @wire(getOpp) wiredOpportunity({data,error}){
-          if(this.opp){
-               if (data) {
-                    this.opportunityList = data;
-                    this.case = true;
-                    console.log(data); 
-               } else if (error) {
-                    console.log(error);
-               }
-          }         
+     connectedCallback() {
+         this.showRecord()        
      }
-     @wire(getCase) wiredCase({data,error}){
-          if(this.case){
-               if (data) {
-                    this.caseList = data;
-                    console.log(data); 
-               } else if (error) {
-                    console.log(error);
-               }
-          }         
+     showRecord(){
+           getContact()
+          .then(result => {
+               this.contactList = result;
+          })
+
+          getOpp()
+          .then(result1 => {
+               this.opportunityList = result1;
+          })
+
+          getCase()
+          .then(result2 => {
+               this.caseList = result2;
+          })
      }
 }
