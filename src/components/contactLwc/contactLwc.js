650c960c211cca310187e2baa0cd174917f08329
Jay Chand Yadav <105698104+Jay-chand-yadav-Fexle-May2022@users.noreply.github.com>
2022-09-19 15:52:03 +0530

    Update contactLwc.js

--- src/components/contactLwc/contactLwc.js
+++ src/components/contactLwc/contactLwc.js
@@ -1,8 +1,18 @@
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
 import getContact from '@salesforce/apex/ContactRecord.userContactRecord';
 
 export default class ContactLwc extends LightningElement {
 
+    @track tempList = [];
     @api flexipageRegionWidth;
-    @wire(getContact) contactList;
+    @wire(getContact)
+    wiredData({error, data}){
+        console.log('1st place......');
+        if(data){          
+           data.forEach((item) => {
+                item.Url = '/' + item.Id;
+                this.tempList.push(item);
+            })
+        }
+    }
 }
